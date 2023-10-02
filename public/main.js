968dd6dec01ffb7327a323cae0e91ed1e2f9bcd3
Thindnavdeep <navdeepthind@gmail.com>
2023-10-02 11:12:07 +0530

    change

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
-// const socket = io("http://localhost:4000/", {})
-const socket = io();
+const socket = io("https://chat-web-application-two.vercel.app/", {})
+// const socket = io();
 
 const clientTotal = document.getElementById('clients-total');
 
